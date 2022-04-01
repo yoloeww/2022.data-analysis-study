dc2f18c11b3e009e0c825f893eac1be5f45a50b7
yolo <77111763+yoloeww@users.noreply.github.com>
2022-04-01 23:21:22 +0800

    Add files via upload

---
+++ 2022.4.1.numpy.array.py
@@ -0,0 +1,17 @@
+# -*- codeing = utf-8 -*-
+# @Time : 2022/4/1 22:58
+# @Author : yolo
+# @File : 2022.4.1.numpy.array.py
+# @Software: PyCharm
+
+import numpy as np
+import pandas as pd
+#a=np.array([2,23,4],dtype=np.int64)
+#print(a.dtype)
+#a=np.array([[2,23,4],[2,43,5]])
+#print(a)
+#a=np.arange(10,20,2)
+#a=np.arange(12).reshape(3,4) #三行四列
+#print(a)
+a=np.linspace(1,10,6).reshape(2,3) #线段
+print(a)
