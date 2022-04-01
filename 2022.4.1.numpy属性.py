7b78def24763d75c8b9c84b228ad21bfbc94f5b2
yolo <77111763+yoloeww@users.noreply.github.com>
2022-04-01 23:21:52 +0800

    Add files via upload

---
+++ 2022.4.1.numpy属性.py
@@ -0,0 +1,12 @@
+# -*- codeing = utf-8 -*-
+# @Time : 2022/4/1 22:51
+# @Author : yolo
+# @File : 2022.4.1.numpy属性.py
+# @Software: PyCharm
+
+import numpy as np
+array=np.array([[1,2,3],[2,3,4]])
+print(array)
+print('number of dim:',array.ndim)
+print('shape:',array.shape)#形状
+print('size',array.size) #数量
